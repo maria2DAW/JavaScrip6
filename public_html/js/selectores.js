1c233acbc943c1deecf1c0d252967dc7b5e6d598
Maria <Maria@192.168.1.49>
2018-03-29 01:37:57 +0200

    Selectores "getElementsByTagName", "getElementsByClassName", "getElementById", "getElementsByName"

---
+++ public_html/js/selectores.js
@@ -0,0 +1,17 @@
+//Me devuelve un array con todos los elementos de la etiqueta buscada3
+document.getElementsByTagName('li'); 
+document.getElementsByTagName('li').length;
+document.getElementsByTagName('li')[3];
+
+//Me devuelve un array con todos los elementos de la clase buscada
+document.getElementsByClassName('course-list');
+document.getElementsByClassName('course-list').length;
+
+//Me devuelve un elemento con el id buscado
+document.getElementById("li1");
+
+//Me devuelve un array con todos los elementos con el name buscado
+document.getElementsByName("input");
+document.getElementsByName("input").length;
+document.getElementsByName("input")[0];prototype
+
