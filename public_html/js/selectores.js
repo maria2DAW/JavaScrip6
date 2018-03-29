1c7d806c98978eb5066d2f36302557cb32605abd
Maria <Maria@192.168.1.49>
2018-03-29 02:29:17 +0200

    Nuevos selectores de JS: "querySelectorAll", "querySelector"

--- public_html/js/selectores.js
+++ public_html/js/selectores.js
@@ -15,3 +15,18 @@ document.getElementsByName("input");
 document.getElementsByName("input").length;
 document.getElementsByName("input")[0];
 
+//Estos métodos trabajan con navegadores modernos (cuidado con IE8 o inferior):
+
+//Me devuelve un array con todos los elementos con el selector buscado.
+//Si es un id, me devulelve el elemento que tenga dicho id.
+//Si busco por clase, etiqueta o atributo, me devuelve el primer elemento del documento HTML.
+document.querySelectorAll('claseLi');
+document.querySelectorAll('li');
+document.querySelectorAll('[href]');
+document.querySelectorAll('#li2');
+document.querySelectorAll('#li2')[0];
+
+//Me devuelve un elemento con el selector buscado
+document.querySelector('#li2');
+document.querySelector('#li2').lengh; //Daría 'undefined' porque no es un array
+document.querySelector('li');
